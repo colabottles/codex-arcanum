a99ad15255ec655b8c496e6890fc77028ebb6028
Todd Libby <toddlibby@protonmail.com>
2025-12-24 20:15:13 -0700

    feat: add initial static pages

---
+++ app/pages/about.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <h1>About This Project</h1>
+    <p>
+      Codex Arcanum is a structured, accessible knowledge and campaign management platform for tabletop RPGs. It is designed to help game masters and players organize and share information about their campaigns, settings, characters, and rules. It also will be developed for content publishers in the future as well.
+    </p>
+    <p>
+      This project is open-source and welcomes contributions from the community. If you're interested in contributing, please visit our GitHub repository for more information.
+    </p>
+  </section>
+</template>
