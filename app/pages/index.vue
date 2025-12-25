59032d1e5458ea60879c510d94d5d695bbf4df34
Todd Libby <toddlibby@protonmail.com>
2025-12-24 21:46:21 -0700

    feat: add initial static pages

---
+++ app/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <section>
+    <h1>Codex Arcanum</h1>
+    <p>
+      Codex Arcanum is a multi-tenant documentation and campaign management platform for tabletop RPGs.
+    </p>
+  </section>
+</template>
